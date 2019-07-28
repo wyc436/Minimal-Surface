c23c7a7b3766714789fcccf0cd3123d7e2e4a1b7
wyc345 <17863130535@163.com>
2019-07-28 22:07:59 +0800

    Modify algorithm implementation

--- src/minimal_surface.m
+++ src/minimal_surface.m
@@ -13,58 +13,32 @@ subplot(122); h = trimesh(t, x(:,1), x(:,2), x(:,3), 'edgecolor', 'k'); axis off
 nv=size(x,1);
 nf=size(t,1);
 
-%% Get boundary vertices
-% When the vertices of triangles are disordered
-
-% [vertices(1:nv).numadjpoints]=deal(0);
-% [vertices(1:nv).neighbortriangles]=deal([]);
-% [vertices(1:nv).neighborpoints]=deal([]);
-% 
-% %Get the adjacency trangle facets of each point
-% for i=1:nf
-%     for k=1:3
-%         vertices(t(i,k)).neighbortriangles(end+1)=i;       
-%     end
-% end
-
-% for i=1:nv
-%     for j=1:size(vertices(i).neighbortriangles,2)
-%         temp=vertices(i).neighbortriangles(j);
-%         for k=1:3
-%             if t(temp,k)~=i && not(any(vertices(i).neighborpoints==t(temp,k)))
-%                 vertices(i).neighborpoints=[vertices(i).neighborpoints,t(temp,k)];
-%                 vertices(i).numadjpoints=vertices(i).numadjpoints+1;
-%             elseif t(temp,k)~=i 
-%                 %vertices(i).neighborpoints=setdiff(vertices(i).neighborpoints,t(temp,k));
-%                 vertices(i).numadjpoints=vertices(i).numadjpoints-1;
-%             end
-%         end
-%     end
-% end
-% edgepoints=find([vertices(:).numadjpoints]);
-
-% When the vertices of triangles are ordered
+%% Get boundary vertice id
 Edge = sparse(t, t(:, [2 3 1]), true, nv, nv);
 [edgepoints,e2] = find(xor( Edge, Edge'));
 
-%% TODO: compute Laplacian
-L = laplacian(x, t);
-L(edgepoints(:),:)=0;
-ind=sub2ind([nv,nv],edgepoints(:),edgepoints(:));
-L(ind)=1;
+for i=1:5
+    %% TODO: compute Laplacian
+    L = laplacian(x, t);
+    L(edgepoints(:),:)=0;
+    ind=sub2ind([nv,nv],edgepoints(:),edgepoints(:));
+    L(ind)=1;
 
-%% TODO: compute minimal surface using LOCAL approach
-% L=L./full(diag(L));
-% L = -spdiags(zeros(nv,1), 0, L);
-% L(ind)=1;
-% while norm(x-L*x)>1e-3
-%     x=L*x;
-% end
+    %% TODO: compute minimal surface using LOCAL approach
+%     L=L./full(diag(L));
+%     L = -spdiags(zeros(nv,1), 0, L);
+%     L(ind)=1;
+%     x2=0.7*x+0.3*L*x;
+%     while norm(x-x2)>1e-3
+%         x=x2;
+%         x2=0.7*x+0.3*L*x;
+%         set(h, 'Vertices', x); drawnow; pause(0.01);
+%     end
 
-%% TODO: compute minimal surface using GLOBAL approach
-b=zeros(nv,3);
-b(edgepoints(:),:)=x(edgepoints(:),:);
-x=L\b;
-
+    %% TODO: compute minimal surface using GLOBAL approach
+    b=zeros(nv,3);
+    b(edgepoints(:),:)=x(edgepoints(:),:);
+    x=L\b;
+end
 %% draw mesh in minimal surface iterations
 set(h, 'Vertices', x);
